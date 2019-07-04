5af4b1473f195deea9c02f1c81d022c80abbdcb4
sraddhareddy <k.sraddhareddy@gmail.com>
2019-07-04 11:38:04 +0530

    Update SortedOrder.java

--- src/main/java/com/stackroute/exercise4/SortedOrder.java
+++ src/main/java/com/stackroute/exercise4/SortedOrder.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 public class SortedOrder {
     public String[] sortingString(String string){
-        String[] array=string.split(" ");       //sorting
+        String[] array=string.split(" ");       //sorting by using .sort() method in Arrays class
         Arrays.sort(array);
         return array;
     }
